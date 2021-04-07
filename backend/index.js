7cff3b12c7b0220f4e58be1e3f74c33d0c18f915
JacoboGarcesO <jacobogarcesoquendo@gmail.com>
2021-04-06 20:20:24 -0500

    Documentación inicial con Swagger

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,35 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const cors = require("cors");
+const swaggerUI = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Geek Cosmetics API",
+            version: "1.0.0",
+            description: "Documentation by Geek Cosmetics API"
+        },
+        servers:[
+            {
+                url: "http://localhost:7007"
+            }
+        ],
+    },
+    apis: ["./src/router/router.js"]
+}
+
+const specs = swaggerJsDoc(options)
 
 app.use(express.json());
 app.use(morgan('dev'));
 require('dotenv').config();
 app.use(cors());
 
-app.use("/", require("./src/router/router"))
+app.use("/", require("./src/router/router"));
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs))
 
 app.set('port', process.env.PORT || 7005);
 
--- backend/src/router/router.js
+++ backend/src/router/router.js
@@ -1,10 +1,109 @@
-const {Router} = require('express');
-const {connection} = require('../../config/db_config');
+const { Router } = require('express');
+const { connection } = require('../../config/db_config');
 const router = Router();
 
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     articulo:
+ *          type: object
+ *          required: 
+ *              - descripcion
+ *              - precio
+ *              - cantidad
+ *          properties:
+ *              id:
+ *                 type: integer
+ *                 description: Identificador de un artículo.
+ *              descripcion: 
+ *                 type: string
+ *                 description: Información adicional sobre el artículo.
+ *              precio: 
+ *                 type: integer
+ *                 description: Valor a pagar por el artículo.
+ *              cantidad: 
+ *                 type: integer
+ *                 description: Cantidad de unidades disponibles de dicho artículo.
+ *          example:
+ *              id: 1
+ *              descripcion: Crema de durazno para la piel
+ *              precio: 40000
+ *              cantidad: 362
+ *     
+ *     compra:
+ *          type: object
+ *          required: 
+ *              - nombre_comprador
+ *              - fecha_compra
+ *              - total_pago
+ *          properties:
+ *              id:
+ *                 type: integer
+ *                 description: Identificador de la compra.
+ *              nombre_comprador: 
+ *                 type: string
+ *                 description: Persona que realiza la compra.
+ *              fecha_compra: 
+ *                 type: date
+ *                 description: Fecha en que se realiza la compra.
+ *              total_pago: 
+ *                 type: float
+ *                 description: Cantidad de dinero total que se debe pagar por la compra.
+ *          example:
+ *              id: 1
+ *              nombre_comprador: Jacobo
+ *              fecha_compra: 01-12-2021T23:59:59.997
+ *              total_pago: 87000.43
+ * 
+ *     articulo_compra:
+ *          type: object
+ *          required: 
+ *              - id_articulo
+ *              - id_compra
+ *              - cantidad
+ *              - subtotal
+ *          properties:
+ *              id:
+ *                 type: integer
+ *                 description: Identificador de la entidad relacional articulo_compra.
+ *              id_articulo: 
+ *                 type: integer
+ *                 description: Identificador del articulo que está en una compra.
+ *              id_compra: 
+ *                 type: integer
+ *                 description: Identificador de una compra.
+ *              cantidad: 
+ *                 type: integer
+ *                 description: Cantidad de unidades de dicho artículo.
+ *              subtotal: 
+ *                 type: float
+ *                 description: Cantidad de dinero dispuesto a pagar por el artículo incluído en la compra multiplicado por la cantidad de unidades.
+ *          example:
+ *              id: 1
+ *              id_articulo: 1
+ *              id_compra: 1
+ *              cantidad: 362
+ *              subtotal: 40501.43
+ */
 
-router.get("/", (req, res)=>{
-    return res.send('Conexión exitosa');
+/**
+ * @swagger
+ * /:
+ *  get:
+ *      summary: Retorna el texto Conexión exitosa
+ *      responses:
+ *          200:
+ *              description: Conexión exitosa de prueba
+ *              content:
+ *                  application/json: 
+ *                          schema:
+ *                              type: json
+ *                              items: #/components/schemas/articulo
+ *                           
+ */                 
+router.get("/", (req, res) => {
+    return res.json('Conexión exitosa');
 })
 
 
